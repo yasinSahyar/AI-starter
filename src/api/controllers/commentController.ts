e9bc24d02f9716d8e32fd25d0b724d0bea64af27
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-08-15 12:26:58 +0300

    Update commentController.ts

--- src/api/controllers/commentController.ts
+++ src/api/controllers/commentController.ts
@@ -10,7 +10,7 @@ const commentPost = async (
       // TODO: Generate a response to a Youtube comment
       // Instead of using openai library, use fetchData to make a post request to the server.
       // see https://platform.openai.com/docs/api-reference/chat/create for more information
-      // You don't need an API key if you use the URL provided in .env.sample and Metropolia VPN
+      // You don't need an API key if you put the URL provided in Oma to .env.sample and Metropolia VPN
       // Example: instad of https://api.openai.com/v1/chat/completions use process.env.OPENAI_API_URL + '/v1/chat/completions'
     } catch (error) {
     next(error);
